d8178b23812ffab4f0ec13d63ff512c7326165ad
sunnyboy30 <74073756+sunnyboy30@users.noreply.github.com>
2021-01-13 17:10:02 +0100

    Added gif and updated README
    Added a way to make a .gif of the performance and updated the README

---
+++ save_as_gif.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 13 17:07:53 2021
+
+@author: grego
+"""
+
+def save_frames_as_gif(frames, path='./', filename='gym_animation.gif'):
+
+    #Mess with this to change frame size
+    plt.figure(figsize=(frames[0].shape[1] / 72.0, frames[0].shape[0] / 72.0), dpi=72)
+
+    patch = plt.imshow(frames[0])
+    plt.axis('off')
+
+    def animate(i):
+        patch.set_data(frames[i])
+
+    anim = animation.FuncAnimation(plt.gcf(), animate, frames = len(frames), interval=50)
+    anim.save(path + filename, writer='Pillow', fps=300)
